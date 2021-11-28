5ac1e4fb9b0681e6fd1f024f036e0746f0217c3e
kenngkz <78695602+kenngkz@users.noreply.github.com>
2021-11-28 17:20:11 +0000

    Add files via upload

---
+++ worker/jobs/eval_model.py
@@ -0,0 +1,133 @@
+from base_utils import path_join
+from constants import WORKER_DATABASE_DIR
+
+import os
+import random
+from importlib import import_module
+
+from stable_baselines3 import PPO
+from luxai2021.env.lux_env import LuxEnvironment
+from luxai2021.game.constants import LuxMatchConfigs_Default
+
+def eval_model(
+    model_path,
+    opp_paths:list,
+    n_games=5,
+    max_steps=1000,
+    seeds=None,
+    replay_dir="eval_replays/",
+    replay_prefix="replay",
+    save_replays=False,
+):
+    '''
+    Evaluate a model by running games against a set of opponents and returns the games results.
+    '''
+    # Set seeds
+    if not seeds:
+        seeds = [random.randint(0, 10000) for _ in range(n_games)]
+    assert len(seeds) == n_games, "Length of 'seeds' arg does not match n_games."
+
+    # Set env configs
+    configs = LuxMatchConfigs_Default
+
+    # Initialize player
+    with open(path_join(WORKER_DATABASE_DIR, model_path, 'info.json'), 'r') as f:
+        model_info = eval(f.read())
+    policy = model_info['train_params']['policy']
+    policy_obj = import_module(f"worker.models.{policy}").AgentPolicy
+    player = policy_obj(mode='train')
+    model=PPO.load(path_join(WORKER_DATABASE_DIR, model_path, "model.zip"))
+
+    print(f"Evaluation for model {model_info['run_id']} against {len(opp_paths)} opponents...")
+
+    # Loop through opponents and run multiple games against them
+    n_opps = len(opp_paths)
+    results = {}
+    for index, opp_path in enumerate(opp_paths):
+        # Initialize opponent
+        assert os.path.exists(path_join(WORKER_DATABASE_DIR, opp_path, 'info.json')), f"File not found: {path_join(opp_path, 'info.json')}"
+        with open(path_join(WORKER_DATABASE_DIR, opp_path, 'info.json'), 'r') as f:
+            opp_info = eval(f.read())
+        opp_id = opp_info['run_id']
+        opp_policy = opp_info['train_params']['policy']
+        opp_policy_obj = import_module('worker.models.' + opp_policy).AgentPolicy
+        opponent = opp_policy_obj(mode='inference', model=PPO.load(path_join(WORKER_DATABASE_DIR, opp_path,'model.zip')))
+
+        match_wins = []
+        opp_performance_summary = {}
+        # Run games and save replay
+        for n in range(n_games):
+            configs['seed'] = seeds[n]
+            if save_replays:
+                replay_name = f'{replay_prefix}_{opp_id}_{n}'
+                env = LuxEnvironment(configs, player, opponent, replay_folder=path_join(WORKER_DATABASE_DIR, model_path, replay_dir), replay_prefix=replay_name)
+            else:
+                env = LuxEnvironment(configs, player, opponent)
+            obs = env.reset()
+            for i in range(max_steps):
+                action_code, _states = model.predict(obs, deterministic=True)
+                obs, rewards, done, info = env.step(action_code)
+                if done:
+                    match_wins.append(int(env.game.get_winning_team() == player.team))
+                    obs = env.reset()
+                    break
+
+            n_wins = sum(match_wins)
+        print(f"All matches against opponent {opp_id} completed. Win - Loss : {n_wins} - {n_games - n_wins}. Progress: {index+1} / {n_opps}")
+
+        # add matches to evaluation match history
+        opp_performance_summary["n_games"] = n_games
+        opp_performance_summary["n_wins"] = n_wins
+        opp_performance_summary["winrate"] = opp_performance_summary["n_wins"]/opp_performance_summary["n_games"]
+
+        results[opp_id] = opp_performance_summary
+
+    print(f"Evaluation for model {model_info['run_id']} complete.")
+    total_games = n_games * n_opps
+    total_wins = sum([summ["n_wins"] for summ in results.values()])
+    print(f"Results -> Total Wins - Total Losses : {total_wins} - {total_games-total_wins}")
+
+    return results
+
+def random_result(
+    model_path,
+    opp_paths:list,
+    n_games=5,
+    max_steps=1000,
+    seeds=None,
+    replay_dir="eval_replays/",
+    replay_prefix="replay",
+    save_replays=False,
+):
+    '''
+    Generate random wins/losses for one eval_model.
+    '''
+    with open(path_join(WORKER_DATABASE_DIR, model_path, 'info.json'), 'r') as f:
+        model_info = eval(f.read())
+    n_opps = len(opp_paths)
+    results = {}
+    for index, opp_path in enumerate(opp_paths):
+        # Initialize opponent
+        assert os.path.exists(path_join(WORKER_DATABASE_DIR, opp_path, 'info.json')), f"File not found: {path_join(opp_path, 'info.json')}"
+        with open(path_join(WORKER_DATABASE_DIR, opp_path, 'info.json'), 'r') as f:
+            opp_info = eval(f.read())
+        opp_id = opp_info['run_id']
+
+        match_wins = []
+        opp_performance_summary = {}
+        # Run games and save replay
+        for n in range(n_games):
+            match_wins.append(random.choice([0, 1]))
+            n_wins = sum(match_wins)
+        # add matches to evaluation match history
+        opp_performance_summary["n_games"] = n_games
+        opp_performance_summary["n_wins"] = n_wins
+        opp_performance_summary["winrate"] = opp_performance_summary["n_wins"]/opp_performance_summary["n_games"]
+
+        results[opp_id] = opp_performance_summary
+
+    print(f"Evaluation (randomly generated) for model {model_info['run_id']} complete.")
+    total_games = n_games * n_opps
+    total_wins = sum([summ["n_wins"] for summ in results.values()])
+    print(f"Results -> Total Wins - Total Losses : {total_wins} - {total_games-total_wins}")
+    return results
---
+++ worker/jobs/eval_stage.py
@@ -0,0 +1,45 @@
+from sys import path
+path.append("C:\\Users\\lenovo\\Desktop\\Coding\\CustomModules")  # allows custom_modules to be imported
+
+from data_structures.quickselect import select_k
+from base_utils import path_join
+
+import os
+
+class ModelScore:
+    '''
+    Links model_id to score. Used in the quickselect algorithm (select_k()).
+    '''
+    def __init__(self, model_id, score):
+        self.id = model_id
+        self.score = score
+
+    def __ge__(self, other):
+        return self.score >= other.score
+
+    def __le__(self, other):
+        return self.score <= other.score
+
+def eval_stage(model_path, eval_results, n_select, n_benchmarks):
+    '''
+    Return the top few models ids and a list of benchmark models ids from eval results.
+    '''
+    # Calculate average winrates for all models in eval_results
+    winrates = {model_id:[eval_results[model_id][opp_id]["winrate"] for opp_id in eval_results[model_id]] for model_id in eval_results}
+    avg_winrates = {model_id:sum(winrates[model_id]) for model_id in winrates}
+    model_scores = [ModelScore(model_id, avg_winrate) for model_id, avg_winrate in avg_winrates.items()]
+
+    # Select the top 'n_select' models
+    _, i, sorted_scores = select_k(model_scores, n_select)
+    best_models = sorted_scores[:i+1]
+    best_models = [model_score.id for model_score in best_models]
+
+    # Select the models to be used as benchmarks
+    stage = os.path.dirname(model_path)
+    _, i, sorted_scores = select_k(model_scores, n_benchmarks)
+    benchmark_models = sorted_scores[:i+1]
+    benchmark_models = [path_join(stage, model_score.id) for model_score in benchmark_models]
+
+    print(f"Eval stage complete. Best models: {best_models}")
+
+    return best_models, benchmark_models
