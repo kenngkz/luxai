8e126e9981484b214c1ea6b8d8ff5c5ff01fbcfd
kenngkz <78695602+kenngkz@users.noreply.github.com>
2021-11-28 17:13:03 +0000

    Clear project files

--- models/agent_blank.py
+++
@@ -1,8 +0,0 @@
-from luxai2021.env.agent import Agent
-
-class AgentPolicy(Agent):
-    '''
-    Add the blank Agent class from luxai2021 to models.
-    '''
-    def __init__(self, *args, **kwargs):
-        super().__init__()
